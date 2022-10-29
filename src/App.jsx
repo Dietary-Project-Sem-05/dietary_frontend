e38861d6306e0d1b321b8e3a2967a083c7ab0d4d
Chamod Madhusanka <chamodmadushanka115@gmail.com>
2022-10-29 16:50:59 +0530

    create moderator dashboard and mod user info page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminUserInfo from "./pages/AdminUserInfo";
@@ -7,18 +7,23 @@ import ModeratorInfo from "./pages/ModeratorInfo";
 import AddModeratorPage from "./pages/AddModeratorPage";
 import LoginPage from "./pages/LoginPage";
 import ModeratorDashboard from "./pages/ModeratorDashboard";
+import ModeratorUserInfo from "./pages/ModeratorUserInfo";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<HomePage/>} exact />
-      <Route path='/admin/dashboard' element={<AdminDashboard/>} exact />
-      <Route path='/admin/info' element={<AdminUserInfo/>} exact />
-      <Route path='/admin/mod-info' element={<ModeratorInfo/>} exact />
-      <Route path='/admin/add-moderator' element={<AddModeratorPage/>} exact />
-      <Route path="/login" element={<LoginPage/>} exact />
-      <Route path="/moderator-dashboard" element={<ModeratorDashboard/>} exact />
-
+      <Route path="/" element={<HomePage />} exact />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} exact />
+      <Route path="/admin/info" element={<AdminUserInfo />} exact />
+      <Route path="/admin/mod-info" element={<ModeratorInfo />} exact />
+      <Route path="/admin/add-moderator" element={<AddModeratorPage />} exact />
+      <Route path="/login" element={<LoginPage />} exact />
+      <Route
+        path="/moderator/dashboard"
+        element={<ModeratorDashboard />}
+        exact
+      />
+      <Route path="/moderator/info" element={<ModeratorUserInfo />} exact />
     </Routes>
   );
 }
--- src/pages/ModeratorUserInfo/index.jsx
+++ src/pages/ModeratorUserInfo/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ModeratorNavBar from "../../components/ModeratorNavBar";
 
-export default function ModeratorDashboard() {
+export default function ModeratorUserInfo() {
   return (
     <div>
       <ModeratorNavBar />
-      <h1>Moderator Dashboard</h1>
+      <h1>Moderator User Information</h1>
     </div>
   );
 }
