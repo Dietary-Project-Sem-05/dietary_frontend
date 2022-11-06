3f5a0347de14b2f15446b182f1e609d8921f23e7
Chamod Madhusanka <chamodmadushanka115@gmail.com>
2022-11-06 16:15:40 +0530

    done unit testing for login page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -11,9 +11,13 @@ import HeightBox from "../../components/HeightBox";
 import loginImg from "../../assets/login.svg";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
+import { Typography } from "antd";
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().email().required().label("Email"),
+  email: Yup.string()
+    .email()
+    .required()
+    .label("Email"),
   password: Yup.string()
     .min(8)
     .max(15)
@@ -35,7 +39,7 @@ const validationSchema = Yup.object().shape({
 });
 
 export default function LoginPage() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
 
   const initialValues = {
@@ -51,8 +55,13 @@ export default function LoginPage() {
         onSubmit=""
       >
         {(formikProps) => {
-          const { values, handleChange, handleSubmit, errors, touched } =
-            formikProps;
+          const {
+            values,
+            handleChange,
+            handleSubmit,
+            errors,
+            touched,
+          } = formikProps;
 
           return (
             <Box
@@ -73,61 +82,69 @@ export default function LoginPage() {
                   />
                   <h1>DIETARY</h1>
                 </Box>
-
-                <Grid
-                  container
-                  alignItems="center"
-                  justify="center"
-                  direction="column"
-                >
-                  <Grid item>
-                    <TextField
-                      size="small"
-                      required
-                      id="email"
-                      name="email"
-                      label="Email"
-                      variant="outlined"
-                      sx={{ width: 300 }}
-                      value={values.email}
-                      onChange={handleChange("email")}
-                      helperText={
-                        touched.email && errors.email ? errors.email : ""
-                      }
-                      error={errors.email}
-                    />
-                  </Grid>
-                  <HeightBox height={15} />
-                  <Grid item>
-                    <TextField
-                      size="small"
-                      required
-                      id="password"
-                      name="password"
-                      label="Password"
-                      variant="outlined"
-                      sx={{ width: 300 }}
-                      value={values.password}
-                      helperText={touched.password ? errors.password : ""}
-                      error={errors.password}
-                      onChange={handleChange("password")}
-                    />
-                  </Grid>
-                  <HeightBox height={15} />
-                  <Button
-                    size="small"
-                    variant="contained"
-                    sx={{ width: 300, backgroundColor: "#636cff" }}
-                    onClick={handleSubmit}
-                    disabled={isLoading}
+                <form data-testid="form">
+                  <Grid
+                    container
+                    alignItems="center"
+                    justify="center"
+                    direction="column"
                   >
-                    {isLoading ? <CircularProgress /> : "LOGIN"}
-                  </Button>
-                  <HeightBox height={5} />
-                  <Link href="#" variant="body2" onClick={() => {}}>
-                    Forgot password?
-                  </Link>
-                </Grid>
+                    <Grid item>
+                      <TextField
+                        size="small"
+                        required
+                        id="email"
+                        name="email"
+                        label="Email"
+                        variant="outlined"
+                        sx={{ width: 300 }}
+                        value={values.email}
+                        onChange={handleChange("email")}
+                        helperText={
+                          touched.email && errors.email
+                            ? errors.email
+                            : "Email is required"
+                        }
+                        error={!!errors.email}
+                      />
+                    </Grid>
+                    <HeightBox height={15} />
+                    <Grid item>
+                      <TextField
+                        size="small"
+                        required
+                        id="password"
+                        name="password"
+                        label="Password"
+                        variant="outlined"
+                        sx={{ width: 300 }}
+                        value={values.password}
+                        helperText={
+                          touched.password
+                            ? errors.password
+                            : "Password is required"
+                        }
+                        error={!!errors.password}
+                        onChange={handleChange("password")}
+                      />
+                    </Grid>
+                    <HeightBox height={15} />
+                    <Button
+                      type="submit"
+                      size="small"
+                      variant="contained"
+                      sx={{ width: 300, backgroundColor: "#636cff" }}
+                      onClick={handleSubmit}
+                      disabled={isLoading}
+                    >
+                      {isLoading ? <CircularProgress /> : "LOGIN"}
+                    </Button>
+                    <HeightBox height={5} />
+                    <Link href="#" variant="body2" onClick={() => {}}>
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                </form>
               </Box>
             </Box>
           );
