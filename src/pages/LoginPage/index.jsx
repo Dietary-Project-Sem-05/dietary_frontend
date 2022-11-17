e3e1ca2a86c1f55e04c0caa8db24169aa8062327
Chamod Madhusanka <chamodmadushanka115@gmail.com>
2022-11-17 23:58:24 +0530

    modify

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -51,7 +51,7 @@ export default function LoginPage() {
 
   useEffect(() => {
     if (userInfo) {
-      navigate("/");
+      navigate("/moderator/dashboard");
     }
   }, [navigate, userInfo]);
 
